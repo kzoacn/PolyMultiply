c942aa786e19e412bfc23c0cae58dd909a42baa9
kzoacn <kzoacn@outlook.com>
2018-07-27 11:50:41 +0800

    test NTT

---
+++ NTT/test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+#include "param.h"
+#include "ntt.h"
+
+
+uint32_t a[11111];
+uint32_t b[11111];
+
+
+
+
+int main(){
+	int i;
+	a[0]=1;
+	a[1]=1;
+	b[0]=1;
+	b[1]=1;
+	
+	double st=clock();
+	
+	ntt_1024_1536(a);
+	ntt_1024_1536(b);
+	
+	for(int i=0;i<1536;i++){
+		a[i]=(long long)a[i]*b[i]%Q;
+	}
+	intt_1536_1536_inv(a);
+	
+	printf("%.8f\n",(clock()-st)/CLOCKS_PER_SEC);
+	for(i=0;i<10;i++)
+		printf("%d%c",(int)a[i]%Q," \n"[i+1==10]);
+	return 0;
+}
